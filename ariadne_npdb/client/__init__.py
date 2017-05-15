9b84e06099ca0a91779c9cd8cbe74f1f5e56d9b0
Justin Riley <justin.t.riley@gmail.com>
2017-05-15 14:13:23 -0400

    initial minimal ORM layout for npdb

--- ariadne_npdb/client/__init__.py
+++ ariadne_npdb/client/__init__.py
@@ -1,12 +1,32 @@
+import os
+
 import pymongo
 
 
-class Base(object):
-    def __init__(self, db=None):
+class Base(dict):
+    def __init__(self, db=None, collection=None):
         self.db = db
+        self.collection = collection or os.environ
+        self.model_name = self.__class__.__name__.lower()
+
+    @property
+    def _root(self):
+        return getattr(self.db, self.collection)
+
+    @property
+    def model(self):
+        return getattr(self._root, self.model_name)
 
     def connect(self, host, database, auth=None, port=27017, ssl=True):
         mc = pymongo.MongoClient(host, port=port, ssl=ssl)
         self.db = getattr(mc, database)
         if auth:
             self.db.authenticate(auth['username'], auth['password'])
+
+    def save(self, **kwargs):
+        if not self.db:
+            raise Exception('missing db connection - forget to connect?')
+        self.model.insert(kwargs)
+
+    def get_all_ids(self):
+        return [i['id'] for i in self.model.find()]
---
+++ ariadne_npdb/client/trial.py
@@ -0,0 +1,5 @@
+from ariadne_npdb.client import Base
+
+
+class Trial(Base):
+    pass
