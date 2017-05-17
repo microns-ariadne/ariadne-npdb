db375655c479014eba88393af681aa7c511d1e72
Justin Riley <justin.t.riley@gmail.com>
2017-05-17 00:26:33 -0400

    wrap pymongo find and find_one in base class

--- ariadne_npdb/models/__init__.py
+++ ariadne_npdb/models/__init__.py
@@ -28,3 +28,9 @@ class Base(dict):
 
     def get_all_ids(self):
         return [i['id'] for i in self.model.find()]
+
+    def find(self, *args, **kwargs):
+        return self.model.find(*args, **kwargs)
+
+    def find_one(self, *args, **kwargs):
+        return self.model.find_one(*args, **kwargs)
